11a4dfa1439d6d25b4f21ead74ccfef6570733ed
Lawtro37 <98205608+Lawtro37@users.noreply.github.com>
2022-01-30 14:22:29 +1000

    Add files via upload
    read resperety description

---
+++ PRO-C10-Template-main/PRO-C10-Template-main/sketch.js
@@ -0,0 +1,83 @@
+var sea,ship;
+var seaImg,shipImg;
+var sec
+
+function preload(){
+  //uncomment the code to add animation to ship 
+  
+  //shipImg1 = loadAnimation("ship-1.png");
+
+    //if worldSecends worked i could of done it with no anamation changes
+
+  //shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  seaImg = loadImage("sea.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
+}
+
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+  //if worldSecends worked i could of done it with no anamation changes
+
+  //if (sec > 1)
+  //{
+  //  sec = 0
+  //}
+
+  //ship.rotation += 2
+
+  //ship controler
+
+  if(keyIsDown(UP_ARROW))
+  {
+     ship.y -=2;
+  }
+
+  if(keyIsDown(DOWN_ARROW))
+  {
+     ship.y +=2;
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  //uncomment code to reset the background
+  if(sea.x < 0){
+    sea.x = 0;
+    sea.x = sea.width;
+    sea.x = sea.width/8;
+    //sea.y = height;
+  }
+
+ 
+  drawSprites();
+}
